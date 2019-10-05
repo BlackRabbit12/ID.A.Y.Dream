a1ee6acc24541c0d04e3ad9e1c92028fb6a36dca
kellerflint <kflint0068@gmail.com>
2019-10-05 11:38:35 -0700

    add references with button

--- functions.js
+++ functions.js
@@ -1,3 +1,29 @@
+let $ref_count = 0;
+
 $( function() {
     $( "#datepicker" ).datepicker();
 } );
+
+// adds reference div on click
+$("#add-ref").on("click", function(){
+    $("#references").append(
+        '<div class="reference">\n' +
+        '    <label for="ref-name">Name</label>\n' +
+        '    <div class="input-group mb-3">\n' +
+        '        <input type="text" class="form-control" id="ref-name" placeholder="Jane Doe">\n' +
+        '    </div>\n' +
+        '    <label for="ref-email">Email</label>\n' +
+        '    <div class="input-group mb-3">\n' +
+        '        <input type="text" class="form-control" id="ref-email" placeholder="me@someone.com">\n' +
+        '    </div>\n' +
+        '    <label for="ref-relation">Relationship</label>\n' +
+        '    <div class="input-group mb-3">\n' +
+        '        <input type="text" class="form-control" id="ref-relation" placeholder="Father">\n' +
+        '    </div>\n' +
+        '</div>'
+    );
+
+    // update number of references
+    $ref_count++;
+    $("#ref-number").html( $ref_count + "/3");
+});
