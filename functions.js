b9502d1763521e9fedf1418adfceb9abf095196b
kellerflint <kflint0068@gmail.com>
2019-10-07 12:11:16 -0700

    add reference removal

--- functions.js
+++ functions.js
@@ -8,22 +8,30 @@ $( function() {
 $("#add-ref").on("click", function(){
     $("#references").append(
         '<div class="reference">\n' +
-        '    <label for="ref-name">Name</label>\n' +
-        '    <div class="input-group mb-3">\n' +
-        '        <input type="text" class="form-control" id="ref-name" placeholder="Jane Doe">\n' +
-        '    </div>\n' +
-        '    <label for="ref-email">Email</label>\n' +
-        '    <div class="input-group mb-3">\n' +
-        '        <input type="text" class="form-control" id="ref-email" placeholder="me@someone.com">\n' +
-        '    </div>\n' +
-        '    <label for="ref-relation">Relationship</label>\n' +
-        '    <div class="input-group mb-3">\n' +
-        '        <input type="text" class="form-control" id="ref-relation" placeholder="Father">\n' +
-        '    </div>\n' +
-        '</div>'
+        '                    <label for="ref-name">Name</label>\n' +
+        '                    <div class="input-group mb-3">\n' +
+        '                        <input type="text" class="form-control" id="ref-name" placeholder="Jane Doe">\n' +
+        '                    </div>\n' +
+        '                    <label for="ref-email">Email</label>\n' +
+        '                    <div class="input-group mb-3">\n' +
+        '                        <input type="text" class="form-control" id="ref-email" placeholder="me@someone.com">\n' +
+        '                    </div>\n' +
+        '                    <label for="ref-relation">Relationship</label>\n' +
+        '                    <div class="input-group mb-3">\n' +
+        '                        <input type="text" class="form-control" id="ref-relation" placeholder="Father">\n' +
+        '                    </div>\n' +
+        '                    <button type="button" class="btn btn-secondary btn-remove">Remove Reference</button>\n' +
+        '                </div>'
     );
-
-    // update number of references
     $ref_count++;
+    // Removes this reference div on click
+    $(".btn-remove").on("click", function() {
+        $(this).parent().remove();
+        $ref_count--;
+        $("#ref-number").html( $ref_count + "/3");
+    });
+    // update number of references
+
     $("#ref-number").html( $ref_count + "/3");
 });
+
