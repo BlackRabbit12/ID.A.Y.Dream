181ff8889aa8f4d1e89d51fc5d0fb9397fddfdcd
Bree <17735836+blackrabbit12@users.noreply.github.com>
2019-10-13 16:43:59 -0700

    Client Side Validation For All Fields

--- functions.js
+++ functions.js
@@ -71,7 +71,7 @@
 *               from display: none to display: block
 *           If the first radio button 'no' is checked then it ensures the default div css
 *               is reinstated as display: none.
- */
+*/
 $('input[name="youth-experience"]').on('change', function(){
     var experienced = document.getElementsByName('youth-experience');
     for (i=0; i < experienced.length; i++) {
@@ -85,21 +85,37 @@ $('input[name="youth-experience"]').on('change', function(){
     }
 });
 
-// const input = document.querySelector('input');
-//
-// input.addEventListener('input', evt => {
-//     const value = input.value;
-//
-//     if (!value) {
-//         input.dataset.state = '';
-//         return
-//     }
-//
-//     const trimmed = value.trim();
-//
-//     if (trimmed) {
-//         input.dataset.state = 'valid'
-//     } else {
-//         input.dataset.state = 'invalid'
-//     }
-// });
+/*
+* Client side validation checks on all <input>, <select>, <textarea>
+*/
+$(document).ready(function(){
+    $("input[type='text']").keyup(function(event){
+        var valueInput = event.target;
+        valueInput = valueInput.value;
+        console.log(valueInput);
+    });
+
+    $("textarea").keyup(function(event){
+        var valueText = event.target;
+        valueText = valueText.value;
+        console.log(valueText);
+    });
+
+    $("input[type='checkbox']").click(function(event){
+        var valueChecked = event.target;
+        valueChecked = valueChecked.checked;
+        console.log(valueChecked);
+    });
+
+    $("input[type='radio']").click(function(event){
+        var valueButton = event.target;
+        valueButton = valueButton.checked;
+        console.log(valueButton);
+    });
+
+    $("option").click(function(event){
+        var valueOption = event.target;
+        valueOption = valueOption.value;
+        console.log(valueOption);
+    });
+});
