6f5d2e9c443409c0cec13f81325189191ad69170
Shayna Jamieson <jamieson.shayna@gmail.com>
2019-10-26 09:57:12 -0700

    Fix

--- scripts/volunteer_functions.js
+++ scripts/volunteer_functions.js
@@ -19,12 +19,26 @@ function displayForm() {
 
 // assigning form event listeners
 
+<<<<<<< HEAD:volunteer_functions_new.js
 
 // Show textbox if "other" is  selected for "Where would you like to help?"
+=======
+// Show textbox if "other" is selected for "Where would you like to help?"
+>>>>>>> 21628db6ea60945ddcaa1e4579d3f1fdb26d3ac6:scripts/volunteer_functions.js
 
 // Show textbox if "weekends" is selected for "I can help..." in the availability section for specific hours
 
 // Show text box for youth experience explanation if Yes is selected
+
+
+// Clears Reference Fields when 'Clear' button is clicked
+$(".clear-reference").on("click", function() {
+    $card = $(this).parent().parent().parent();
+    $inputs = $card.find("input");
+    for (let i = 0; i < $inputs.length; i++) {
+        $inputs[i].value = "";
+    }
+});
 
 /* --- Form validation --- */
 
@@ -58,89 +72,6 @@ let validatePhoneArray = [
     "ref-phone-3",
     "phone"
 ];
-
-// Validates phone numbers and adds formatting
-// forces valid phone number and returns true if the phone number contains 10 or 11 characters. Displays errors
-function validate_phone(id) {
-    console.log("validate phone");
-    // formats phone number
-    let str = $("#" + id).val();
-    str = str.replace(/\D/g, "");
-
-    if (str.length < 4) {
-        // do nothing
-    } else if (str.length < 7) {
-        str =  "(" + str.substring(0, 3) + ") " + str.substring(3, 6);
-    } else {
-        str =  "(" + str.substring(0, 3) + ") " + str.substring(3, 6) + "-" + str.substring(6, 10);
-    }
-
-    $("#" + id).val(str);
-
-    if (str.length != 14) {
-        $("#err-" + id).removeClass("d-none");
-        $("#" + id).addClass("red-border-drop");
-        return false;
-    } else {
-        $("#err-" + id).addClass("d-none");
-        $("#" + id).removeClass("red-border-drop");
-        return true;
-    }
-}
-
-// checks if given inputs are empty
-function validate_empty(id) {
-    if (!isEmpty($("#" + id).val())) {
-        $("#err-" + id).addClass("d-none");
-        $("#" + id).removeClass("red-border-drop");
-        return true;
-    } else {
-        $("#err-" + id).removeClass("d-none");
-        $("#" + id).addClass("red-border-drop");
-        return false;
-    }
-}
-
-// returns true if email is valid and false otherwise. Displays errors
-function validate_email(id) {
-    let expression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!expression.test(String($("#" + id).val()).toLocaleLowerCase())) {
-        $("#err-" + id).removeClass("d-none");
-        $("#" + id).addClass("red-border-drop");
-        return false;
-    } else {
-        $("#err-" + id).addClass("d-none");
-        $("#" + id).removeClass("red-border-drop");
-        return true;
-    }
-}
-
-// Validates zip code and adds formatting
-function validate_zip() {
-    $("#zip").val($("#zip").val().trim().substring(0, 5));
-    if ($("#zip").val().trim().length < 5) {
-        $("#err-zip").removeClass("d-none");
-        $("#zip").addClass("red-border-drop");
-        return false;
-    } else {
-        $("#err-zip").addClass("d-none");
-        $("#zip").removeClass("red-border-drop");
-        return true;
-    }
-}
-
-// Validates that a T-Shirt size is selected
-function validate_tshirt() {
-    if (document.getElementById("t-shirt-none").selected) {
-        $("#err-t-shirt").removeClass("d-none");
-        $("#t-shirt").addClass("red-border-drop");
-        return false;
-    } else {
-        $("#err-t-shirt").addClass("d-none");
-        $("#t-shirt").removeClass("red-border-drop");
-        return true;
-    }
-}
 
 /* --- Assigning validation event listeners --- */
 
@@ -207,11 +138,4 @@ function validateForm() {
     }
 
     return isValid;
-}
-
-/* --- Helper functions --- */
-
-// returns true if string is empty
-function isEmpty(str) {
-    return str.trim() == "";
 }
