a89cf8b516ee76d8a2d057468216ae3535fef300
Bridget B <17735836+BlackRabbit12@users.noreply.github.com>
2019-10-30 11:46:12 -0700

    Header update
    Header update for more information

--- scripts/youth_splash_functions.js
+++ scripts/youth_splash_functions.js
@@ -1,3 +1,12 @@
+/*
+Authors: Shayna Jamieson, Bridget Black, Keller Flint
+2019-10-29
+Last Update: 2019-10-29
+Version: 1.0
+File Name: youth_splash_functions.js
+Associated File: volunteer_form.html
+                youth_form.html
+*/
 // on click, displays results summary for youth splash page
 document.getElementById("summary-button").onclick = toggleSummary;
 function toggleSummary() {
