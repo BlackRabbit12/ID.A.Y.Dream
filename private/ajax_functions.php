aa167a42d99b7ee8362ca73c9aa2afc375916f1e
baki <bsblack12@gmail.com>
2019-12-01 16:02:49 -0800

    status change working

--- private/ajax_functions.php
+++ private/ajax_functions.php
@@ -23,6 +23,8 @@ if (isset($_POST['status'])){
     $table = substr($_POST['dataSelect'], 0, strlen($_POST['dataSelect']) -1);
     //get table id with the table name + id
     $table_id = $table . "_id";
+    //TODO change to status when dreamer is updated in database
+    $key = $table . "_active";
     //uppercase table name
     $table = strtoupper(substr($table, 0, 1)) . substr($table, 1, strlen($table));
 
@@ -31,6 +33,11 @@ if (isset($_POST['status'])){
     $result = mysqli_query($db, $sql);
     $id = mysqli_fetch_assoc($result)[$table_id];
 
+    //create associative array of data
+    $dataArray[$key] = $_POST['status'];
+
+    //update the changes
+    updateData($table, $table_id, $dataArray, $id);
 } //end isset($_POST['status'])
 
 //if the admin_page.php <select> <option> is selected (not 'none')
