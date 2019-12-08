af2af7a9327f37c0a5bb6bf7ef189730c7f7b660
Shayna Jamieson <jamieson.shayna@gmail.com>
2019-12-08 15:18:30 -0800

    Finish terms validation

--- volunteer_form.php
+++ volunteer_form.php
@@ -446,19 +446,20 @@ require_once "private/init.php";
         <!-- E-Signature -->
         <fieldset class="form-group">
             <div class="container legend-container">
-                <h2 class="legend-text">E-Signature</h2>
+                <h2 class="legend-text">Terms and Conditions</h2>
             </div>
             <div class="row">
                 <div class="col-12">
                     <label class="col-form-label" id="submit-policy">By submitting this application, I certify that my statements in this application are true, complete and correct to the best of my knowledge. I further understand that as a part of the volunteer verification and matching process, additional personal information will be required of me. I hereby authorize iD.A.Y.dream to contact the references listed and to conduct a background check to determine if I will be a good fit as a volunteer for the organization. I understand that submitting this application does not guarantee my participation. I also hereby authorize iD.A.Y.dream without limitation, to copy, publish, exhibit or distribute photographs or video tapes of my volunteer activities for the purpose of promoting volunteerism and support. I release and hold harmless from liability any person or organization that provides information. I also agree to hold harmless iD.A.Y.dream and the officers and volunteers thereof.</label>
                 </div>
             </div> <!-- end of row that contains our signature explanation -->
-            <div class="row">
+            <!-- since getting new terms and conditions we don't actually need to display checkbox -->
+            <div class="row d-none">
                 <div class="col-12">
                     <!--form-check form-check-inline  for div and for input form-check-input  and for label  col-form-label -->
                     <div class="custom-control custom-checkbox">
                         <input class="custom-control-input" type="checkbox" id="information-is-true"
-                               name="terms-of-Service" value="agreed">
+                               name="terms-of-Service" value="agreed" checked>
                         <label class="custom-control-label" for="information-is-true">I have read and agree to the statement above.</label>
                     </div>
                 </div>
